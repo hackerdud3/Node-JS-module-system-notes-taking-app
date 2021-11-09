20d33f5b09d0430a16637e24e4ead81126fc31fc
Vinay <nandikantivinay35@gmail.com>
2021-11-09 13:44:30 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Starting app.js");
+
+const yargs = require('yargs');
+const notes = require('./notes.js');
+const argv = yargs.argv;
+
+var title = yargs.argv.title;
+var body = yargs.argv.body;
+var command = yargs.argv._[0];
+
+if (command === "add") {
+  console.log("New note created!");
+  notes.addingNote(title, body);
+} else if (command === "remove") {
+  console.log("Nore removed");
+  notes.removeNote(title);
+} else if (command === "read") {
+  console.log("reading note");
+  notes.readNote(title);
+} else if (command === "list") {
+  console.log("Your notes");
+  notes.getAll();
+} else {
+  console.log("Unknown command was used!");
+}
